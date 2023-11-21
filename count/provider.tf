78ca4f687f1e9fe87e94d3d5eedb6ef88c455d22
abh1thakur <114871636+abh1thakur@users.noreply.github.com>
2023-11-21 18:27:47 -0500

    Update provider.tf

--- count/provider.tf
+++ count/provider.tf
@@ -8,5 +8,5 @@ terraform {
 provider "aws" {
   region = "us-east-1"
   aws_access_key_id = "AKIA4UIM6RK7H3H25NW4"
-  secret_key = ""
+  aws_secret_access_key = "xfgfxdfgxdr3434536fgjghuty7567dg"
 }
