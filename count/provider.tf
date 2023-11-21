d133e4cc33fc658a02031d9f630fed26dc53aa9d
abh1thakur <114871636+abh1thakur@users.noreply.github.com>
2023-11-21 18:31:32 -0500

    Update provider.tf

--- count/provider.tf
+++ count/provider.tf
@@ -7,6 +7,6 @@ terraform {
 }
 provider "aws" {
   region = "us-east-1"
-  aws_access_key_id = "AKIA4UIM6RK7H3H25NW4"
-  aws_secret_access_key = "xfgfxdfgxdr3434536fgjghuty7567dg"
+  access_key = ""
+  secret_key = ""
 }
